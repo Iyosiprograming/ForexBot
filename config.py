e8d2c1264a0887638c618ce8c13dae0bb267d3dc
Iyosiyas <iyosieyosiyas@gmail.com>
2026-01-28 09:35:50 -0800

    feat:create mt5_connection

---
+++ config.py
@@ -0,0 +1,3 @@
+MT5_Login = 298168794
+MT5_Password = "Manyouare100@"
+MT5_Server = "Exness-MT5Trial9"
--- mt5_connect.py
+++ mt5_connect.py
@@ -1,7 +1,7 @@
 import MetaTrader5 as mt5
-
+from config import MT5_Login, MT5_Password, MT5_Server
 def connect_mt5():
-    if not mt5.initialize():
+    if not mt5.initialize(login=MT5_Login, password=MT5_Password, server=MT5_Server):
         print("MT5 init failed:", mt5.last_error())
         return False
 
@@ -11,6 +11,7 @@ def connect_mt5():
         return False
 
     print(f"Connected to MT5 | Account: {account.login}")
+    print(f"Balance: {account.balance}")
     return True
 
 
